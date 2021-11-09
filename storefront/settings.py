22d0865f96dbe11df148faae60db1e7e2700bba0
RF_BURGER <Arefyazdi4@gmail.com>
2021-11-09 13:16:31 +0330

    runserver

--- storefront/settings.py
+++ storefront/settings.py
@@ -19,9 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-from secret_key import SECRET_KEY as SK
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SK
+SECRET_KEY = 'django-insecure-hhp&lodjf14bm#-ti-!mb@@5*1$aifia-%d=_%co7v$ypkp!-!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
