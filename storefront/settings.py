b10881a752ee442ca40fa923661c6a484cf04419
RF_BURGER <Arefyazdi4@gmail.com>
2022-02-03 01:40:33 +0330

    creating a models app

--- storefront/settings.py
+++ storefront/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'playground',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
+    'playground',
+    'store',
+    'tags'
 ]
 
 MIDDLEWARE = [
