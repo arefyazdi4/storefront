59e76292e80eb0269048c8f7adf2c9d6a8b536a2
RF_BURGER <Arefyazdi4@gmail.com>
2022-02-08 16:38:07 +0330

    Running Migrations

--- store/migrations/0003_add_slug_to_product.py
+++ store/migrations/0003_add_slug_to_product.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('store', '0002_rename_price_to_unit_price'),
+        ('store', '0002_price_to_unit_price'),
     ]
 
     operations = [
