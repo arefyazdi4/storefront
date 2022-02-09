c28827b9dfbd0bfcf57b3ebab808ba6469c8e7db
RF_BURGER <Arefyazdi4@gmail.com>
2022-02-09 11:05:13 +0330

    using mysql in django

--- storefront/settings.py
+++ storefront/settings.py
@@ -85,8 +85,11 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'MyPassword'
     }
 }
 
