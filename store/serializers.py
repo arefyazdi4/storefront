dfd475aac932c03de1afa2f3b62bcc90265f7dd9
RF_BURGER <Arefyazdi4@gmail.com>
2022-04-28 16:59:20 +0430

    12-Model Serializers

--- store/serializers.py
+++ store/serializers.py
@@ -3,26 +3,24 @@ from rest_framework import serializers
 from .models import Product, Collection
 
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+class ProductSerializer(serializers.ModelSerializer):  # change base class to 'ModelSerializer'
+    class Meta:
+        model = Product
+        # fields = to an array or tuples  of fields in product class we wane include here
+        fields = ['id', 'title', 'price_with_tax', 'collection']
+
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
     collection = serializers.HyperlinkedRelatedField(
         queryset=Collection.objects.all(),
         view_name='collection-detail'  # this argument is used for generating hyper links
         # we need to creat a view and url by this name
     )                  # fourth method
-    # collection = CollectionSerializer()  # third method
-    # collection = serializers.PrimaryKeyRelatedField(  # first method
-    #     queryset=Collection.objects.all()
-    # )
-    # collection = serializers.StringRelatedField()  # second method
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
